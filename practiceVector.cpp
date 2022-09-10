7768d863ebe4cd2e2d696c8d130abffa03a5eb22
OmurRifat <jabedomur.ece.ku@gmail.com>
2022-09-10 19:20:15 +0600

    last changes

---
+++ practiceVector.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // vector<string> str;
+    // int n;
+    // string x;
+    // cin >> n;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> x;
+    //     str.push_back(x);
+    // }
+    // for (int i = 0; i < str.size(); i++)
+    // {
+    //     cout << str[i] << endl;
+    // }
+    /*
+    ---------- int vector practiced --------------
+    vector<int> v;
+    int n;
+    int x = 6;
+    cin >> x;
+    for (int i = 0; i < x; i++)
+    {
+        cin >> n;
+        v.push_back(n);
+    }
+    // v.erase(0);//will not work with array index number.
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    v.erase(v.begin() + 2, v.end() - 2);
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    -------------------------------------------------
+    */
+
+       return 0;
+}
